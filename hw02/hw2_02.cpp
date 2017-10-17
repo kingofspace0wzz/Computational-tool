1681efefb39e0137eb89a826ece1d273a46be9dd
kingofspace0wzz <zizhuang_wang@umail.ucsb.edu>
2017-10-17 11:22:21 -0700

    Update

--- hw02/hw2_02.cpp
+++ hw02/hw2_02.cpp
@@ -1,42 +1,32 @@
 #include <complex>
-#include <iostream>
-#include <fstream>
 #include <cstdlib>
-
+#include <fstream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	
-	complex<double> c(0.3, -0.4);
-	complex<double> z;
+int main() {
+	const complex<double> c(0.3, -0.4); // complex number c
+	complex<double> z; // complex number z
+	const double m = max(abs(c), 2.0);
+	ofstream outFile("julia.csv");
+	if (!outFile) {
+		exit(EXIT_FAILURE);
+	}
+	for (double i = -2; i < 2; i += 0.004) {
+		for (double j = -2; j < 2; j += 0.004) {
+			// Initial point
+			z = complex<double>(i, j);
 
-	int count = 0; // for Debugging
-	double m = max(abs(c),2.0);
-	ofstream outFile( "julia.txt");
-	if( !outFile )
-    {
-        exit(1);
-        return -1;
-    }
-	for (int i = 0; i <= 999; ++i)
-	{
-		for (int j = 0; j <= 999; ++j)
-		{
-			z = complex<double> (-2.0 + 4/1000*i, -1.0 + 3/1000*j);
-			
-			for (int k = 0; k < 100; ++k)
-			{
-				z = z*z + c;
-
+			// julia operation for 100 iterations
+			for (int k = 0; k < 100; ++k) {
+				z = z * z + c; // z(n+1) = z(n)^2 + c
 			}
-			count++;
-			if (abs(z) <= m)
-				outFile << real(z) << " " << imag(z) << endl;
+			if (abs(z) <= m) {
+				// Print the real and imaginary part into julia.csv
+				outFile << i << "," << j << endl;
+			}
 		}
 	}
-	cout << count << endl;
-	cout << m;
-
 	outFile.close();
 	return 0;
 }
